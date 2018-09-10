f0f3549d5ae8b516a25de080df3eec0502a3a495
de-los-Code <you@example.com>
2018-09-10 11:50:00 +0200

    Wolfram_updated_version

--- Command_Functions.py
+++ Command_Functions.py
@@ -1,20 +1,23 @@
 import wolframalpha
+from playsound import playsound
 #split_file = voicecommand.split(' ')
 
-def Q_and_A(voicecommand, language):
-    app_id = "A87QVU-TU925YT9UR"
-    client = wolframalpha.Client(app_id)
-
-    try:
-        res = client.query(voicecommand)
-        answer = next(res.results).text
-        return verbalize(answer, language)
-    except:
-        print("No answer. Please ask another question.")
-        answer = "No answer. Please ask another question."
-        return verbalize(answer, language)
-
-def verbalize (mytext, language):
-    myobj = gTTS(text=mytext, lang=language, slow=False)
+def verbalize (mytext, language): # setting up the computer's ability to answer orally
+    myobj = gTTS(text=mytext, lang=language, slow=False) 
     myobj.save("welcome.mp3")
-    os.system("mpg321 welcome.mp3")
+    #os.system("mpg321 welcome.mp3")
+    playsound('welcome.mp3')
+
+def Q_and_A(voicecommand, language): #2 variables: voiceommand = input, language (linguistic setting)
+    app_id = "A87QVU-TU925YT9UR" #from my Wolfram account
+    client = wolframalpha.Client(app_id) #defining me, the voice commander
+
+    try: #sub-function that executes the body.
+        res = client.query(voicecommand) #client asks a question
+        answer = next(res.results).text  #wolfram API responds by text.
+        return verbalize(answer, language) # returns answer orally
+    except: #  the equivalent of 'else' that handles a value error. 
+        print("No answer. Please ask another question.")
+        answer = "No answer. Please ask another question." #v
+        return verbalize(answer, language) # if not understood, returns answer orally
+
